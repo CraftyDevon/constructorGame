3f378ba93591de89c29937419c6303c2a114fd65
Devon Hicks <devon@devonhicks.com>
2014-04-16 13:27:44 -0400

    v1.0

---
+++ js/core.js
@@ -0,0 +1,47 @@
+$( document ).ready(function() {
+
+
+function Wizard(wandType, speciality, alliegance, life){
+  this.wandType = wandType || "elm";
+  this.speciality = speciality || "transfiguration";
+  this.alliegance = alliegance || "Hogwarts";
+  this.life = life || 100;
+  this.heal = function(target){
+    console.log("I've healed myself");
+    this.life = 100;
+  }
+
+}// end wizard function
+
+
+function Wands(woodType, coreMaterial, spell){
+    this.woodType = woodType || "elm";
+    this.coreMaterial = coreMaterial || "Phoenix Feather";
+    this.spell = spell || "DeathStrike";
+    this.cast = function(target){
+      console.log("Castio!");
+    }
+
+}//end wands function
+
+
+function MagicalBeast(name, weakness, ability, life){
+  this.name = name || "creature";
+  this.weakness = weakness || "groin shot";
+  this.ability = ability || "claw";
+  this.life = life || 100;
+  this.maul = function(target){
+    console.log("Mess your shit up");
+  }
+
+
+}//end MagicalBeast function
+
+
+
+
+
+
+
+
+});

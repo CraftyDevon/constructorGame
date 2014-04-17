5ec653163ca6dc0e2335ffe71c37a99b6df388df
Devon Hicks <devon@devonhicks.com>
2014-04-16 23:40:44 -0400

    Version 1.3, Dragon and Wizard can attack each other, Wizard can self heal

--- js/core.js
+++ js/core.js
@@ -1,38 +1,100 @@
-
-function Wizard(wandType, speciality, alliegance, life){
+function Wizard(wandType, speciality, alliegance, wizLife){
   this.wandType = wandType || "elm";
   this.speciality = speciality || "transfiguration";
   this.alliegance = alliegance || "Hogwarts";
-  this.life = life || 100;
-  this.heal = function(target){
-    console.log("I've healed myself");
-    this.life = 100;
-  }
+  this.wizLife = wizLife || 100;
+
+    this.addDamage = function(dragon) {
+      if (this.wizLife <= 0) {
+        console.log("im in add damage if less eq to this.life");
+        this.ability = "none";
+        alert("You're dead");
+      }
+      this.wizLife = this.wizLife - dragon.damage;
+
+    };
+
+      this.heal = function(target){
+    $( ".healcasttext" ).append( "<h4>You've healed yourself!</h4>" );
+    this.wizLife = 100;
+      
+
+
+}
 
 }// end wizard function
 
-var wizard = new Wizard();
-
-
-function Wands(woodType, coreMaterial, spell){
+function Wand(woodType, coreMaterial, spell, damage){
     this.woodType = woodType || "elm";
     this.coreMaterial = coreMaterial || "Phoenix Feather";
     this.spell = spell || "DeathStrike";
-    this.cast = function(target){
-      console.log("Castio!");
-    }
+    this.damage = damage || 50;
+  
+      this.wizCast = function(target) {
+       $( ".spellcasttext" ).append( "<h4>You attack the Hungarian Horntail!!</h4>" );
+
+      var chance = Math.floor(Math.random() * 10);
+      console.log(chance);
+      if (chance < 3) {
+        target.addWizDamage(this);
+      }
+    };
 
 }//end wands function
 
 
-function Dragon(name, weakness, ability, life){
+
+
+function Dragon(name, weakness, ability, life, damage){
   this.name = name || "Norbert";
   this.weakness = weakness || "groin shot";
   this.ability = ability || "claw";
   this.life = life || 100;
-  this.maul = function(target){
-    console.log("Mess your shit up");
-  }
+  this.damage = damage || 30;
 
+this.maul = function(target) {
+        $( ".dragonmaul" ).append( "<h4>The Hungarian Horntail mauls you!</h4>" );
+      var chance = Math.floor(Math.random() * 10);
+      console.log(chance);
+      if (chance < 3) {
+        target.addDamage(this);
+      }
+    };
+
+      this.addWizDamage = function(wiz) {
+      if (this.life <= 0) {
+        console.log("im in add damage if less eq to this.life");
+        this.ability = "none";
+        alert("You killed the Hungarian Horntail!");
+      }
+      this.life = this.life - wiz.damage;
+
+    };
 
 }//end MagicalBeast function
+
+var harryPotter = new Wizard();
+
+var ginnyWand = new Wand();
+
+var hungarianHorntail = new Dragon();
+
+function heal(target){
+    this.wizLife = 100;
+    $( ".healcasttext" ).append( "<h4>You've healed yourself!</h4>" );
+};
+
+function spellCast(target){
+  this.life = this.life - 50;
+      $( ".spellcasttext" ).append( "<h4>You cast the deathstrike curse!</h4>" );
+
+};
+
+function dragonMaul(target){
+        $( ".dragonmaul" ).append( "<h4>The Hungarian Horntail mauls you!</h4>" );
+
+}
+
+
+
+
